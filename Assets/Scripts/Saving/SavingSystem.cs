fea5ee4ee18b5872da6c632b3d0ccf7250da7158
Anthony Howe <anthowe33@gmail.com>
2020-05-26 21:10:02 -0400

    Try byte coding

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using System.Text;
 
 namespace RPG.Saving
 {
@@ -9,12 +11,34 @@ namespace RPG.Saving
 
         public void Save(string saveFile)
         {
-            print("Saving to " + saveFile);
+            string path = GetPathFromSaveFile(saveFile);
+            print("Saving to " + path);
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("¡Hola Mando!");
+                stream.Write(bytes, 0, bytes.Length);
+            }
+     
         }
 
         public void Load(string saveFile)
         {
-            print("Loading from " + saveFile);
+            string path = GetPathFromSaveFile(saveFile);
+            print("Loading from " + path);
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                print (Encoding.UTF8.GetString(buffer));
+
+            }
+        }
+
+        private string GetPathFromSaveFile(string saveFile)
+        {
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
+           // print("File is " + saveFile);
+           // "/Users/anthonyhowe/Library/Application Support/DefaultCompany/RPG"
         }
     }
 }
